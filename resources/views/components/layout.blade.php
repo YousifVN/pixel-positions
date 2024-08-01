005d036abdc815d2146f1b0f37412b2d4c528a7a
YousifVN <yousifmahmood995@gmail.com>
2024-08-01 03:42:11 +0300

    finished most of the design

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,14 +1,18 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pixel Positions</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@400;500;600&display=swap" rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="text-white bg-black">
+<body class="text-white bg-black font-hanken-grotesk">
     <div class="px-10">
         <nav class="flex items-center justify-between py-4 border-b border-white/10">
             <div>
@@ -16,15 +20,16 @@
                     <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
                 </a>
             </div>
+
             <div class="space-x-6 font-bold">
                 <a href="#">Jobs</a>
                 <a href="#">Careers</a>
                 <a href="#">Salaries</a>
                 <a href="#">Companies</a>
-
             </div>
+
             <div>
-                <a href="#">Post a Job</a>
+                <a href="">Post a Job</a>
             </div>
         </nav>
 
@@ -32,6 +37,7 @@
             {{ $slot }}
         </main>
     </div>
+
 </body>
 
 </html>
