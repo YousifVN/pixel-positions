82c319e190910a732d3a3e366a4cac7c3141c7b7
YousifVN <yousifmahmood995@gmail.com>
2024-08-01 21:45:32 +0300

    half way done with the backend

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,19 +1,21 @@
+@props(['job'])
+
 <x-panel class="flex gap-x-6">
     <div>
         <x-employer-logo />
     </div>
 
     <div class="flex flex-col flex-1">
-        <a href="#" class="self-start text-sm text-gray-400 transition-colors duration-300">Laracasts</a>
+        <a href="#" class="self-start text-sm text-gray-400 transition-colors duration-300">{{ $job->employer->name }}</a>
 
-        <h3 class="mt-3 text-xl font-bold group-hover:text-blue-800">Video Producer</h3>
+        <h3 class="mt-3 text-xl font-bold group-hover:text-blue-800">{{ $job->title }}</h3>
 
-        <p class="mt-auto text-sm text-gray-400">Full Time - From $60,000</p>
+        <p class="mt-auto text-sm text-gray-400">{{ $job->salary }}</p>
     </div>
 
     <div>
-        <x-tag>Tag</x-tag>
-        <x-tag>Tag</x-tag>
-        <x-tag>Tag</x-tag>
+        @foreach($job->tags as $tag)
+            <x-tag :$tag />
+        @endforeach
     </div>
 </x-panel>
